677e70998bc57041529f003a7530c69b91694374
gazzer82 <contact@garethjeanne.co.uk>
2016-01-20 14:50:55 -0800

    updated JSON url to use https

--- src/actions/action_fetch_campers.jsx
+++ src/actions/action_fetch_campers.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const FETCH_CAMPERS = 'FETCH_CAMPERS';
 
-const FCC_URL = 'http://fcctop100.herokuapp.com/api/fccusers/top/recent'
+const FCC_URL = 'https://fcctop100.herokuapp.com/api/fccusers/top/recent'
 
 export function fetchCampers(){
   const request = axios.get(FCC_URL);
